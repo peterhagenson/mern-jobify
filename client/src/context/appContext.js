ac7b0d8f67608d4eb47159ec3364f4a9242fbd76
Peter Hagenson <p.hagenson@gmail.com>
2023-01-14 15:57:29 -0600

    saved user info in local storage after registration

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -5,17 +5,19 @@ import {
     DISPLAY_ALERT, CLEAR_ALERT, REGISTER_USER_BEGIN, REGISTER_USER_ERROR, REGISTER_USER_SUCCESS
 } from "./actions";
 
-
+const token = localStorage.getItem('token')
+const user = localStorage.getItem('user')
+const userLocation = localStorage.getItem('location')
 
 const initialState = {
     isLoading: false,
     showAlert: false,
     alterText: '',
     alterType: '',
-    user: null,
-    token: null,
-    userLocation: '',
-    jobLocation: ''
+    user: user ? JSON.parse(user) : null,
+    token: token,
+    userLocation: userLocation || '',
+    jobLocation: userLocation || '',
 }
 
 const AppContext = React.createContext()
@@ -35,6 +37,19 @@ const AppProvider = ({ children }) => {
         }, 3000)
     }
 
+    const addUserToLocalStorage = ({ user, token, location }) => {
+        localStorage.setItem('user', JSON.stringify(user))
+        localStorage.setItem('token', token)
+        localStorage.setItem('location', location)
+    }
+
+    const removeUserFromLocalStorage = () => {
+        localStorage.removeItem('token')
+        localStorage.removeItem('user')
+        localStorage.removeItem('location')
+
+    }
+
     const registerUser = async (currentUser) => {
         dispatch({ type: REGISTER_USER_BEGIN })
         try {
@@ -42,7 +57,7 @@ const AppProvider = ({ children }) => {
             console.log(response);
             const { user, token, location } = response.data
             dispatch({ type: REGISTER_USER_SUCCESS, payload: { user, token, location } })
-            //local storage later
+            addUserToLocalStorage({ user, token, location })
         } catch (error) {
             console.log(error.response);
             dispatch({ type: REGISTER_USER_ERROR, payload: { msg: error.response.data.msg }, })
